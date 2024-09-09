93d75f27e2b8c26bad1618d3f624fb22ff1d4b54
Arvel <2381024@unai.edu>
2024-09-09 21:19:05 +0700

    Handson2

--- latihan/Soal3.java
+++ latihan/Soal3.java
@@ -6,7 +6,8 @@ public class Soal3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int hasil;
+        int hasil = 0;
+        String operand = "";
 
         System.out.println("Masukkan angka pertama");
         int a = input.nextInt();
@@ -18,8 +19,27 @@ public class Soal3 {
         int x = input.nextInt();
         switch (x){
             case 1:
+                hasil = a * b;
+                operand = " * ";
+                break;
+            case 2:
+                hasil = a / b;
+                operand = " / ";
+                break;
+            case 3:
                 hasil = a + b;
+                operand = " + ";
+                break;
+            case 4:
+                hasil = a - b;
+                operand = " - ";
+                break;
+            case 5:
+                hasil = a % b;
+                operand = " % ";
                 break;
         }
+
+        System.out.println(a + operand + b + " = " + hasil);
     }
 }
